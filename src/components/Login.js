94582c85c34478bb987602f24a0821a8137c6fc2
Molik-Singla <77623428+Molik-Singla@users.noreply.github.com>
2022-04-10 16:33:15 +0530

    First Commit

---
+++ src/components/Login.js
@@ -0,0 +1,85 @@
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { LoginContext } from "./../context/LoginContext";
+
+toast.configure();
+function Login() {
+    console.log("Login");
+    const navi = useNavigate();
+
+    const [is_login, setIsLogin] = useContext(LoginContext);
+
+    const [form_data, setFormData] = useState({
+        name: "",
+        password: "",
+    });
+
+    function handleForm(evt) {
+        const { name, value } = evt.target;
+        setFormData((prev) => {
+            return {
+                ...prev,
+                [name]: value,
+            };
+        });
+    }
+    function handleSubmit() {
+        if (localStorage.getItem("login")) {
+            const { name, password } = JSON.parse(localStorage.getItem("login"));
+            if (name === form_data.name && password === form_data.password) {
+                setIsLogin(true);
+                navi("/home");
+            } else
+                toast.error("Login Failed!", {
+                    position: "top-center",
+                    autoClose: 1800,
+                });
+        }
+    }
+    return (
+        <div className="w-screen h-screen flex items-center justify-center">
+            <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+                <div className="mb-4">
+                    <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="username">
+                        Username
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                        id="username"
+                        type="text"
+                        placeholder="Username"
+                        name="name"
+                        onChange={handleForm}
+                        value={form_data.name}
+                    />
+                </div>
+                <div className="mb-6">
+                    <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="password">
+                        Password
+                    </label>
+                    <input
+                        className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+                        id="password"
+                        type="password"
+                        placeholder="******************"
+                        name="password"
+                        onChange={handleForm}
+                        value={form_data.password}
+                    />
+                    <p className="text-red text-xs italic">Please enter your password.</p>
+                </div>
+                <div className="flex items-center justify-between">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2" onClick={handleSubmit}>
+                        Login
+                    </button>
+                </div>
+            </div>
+            <ToastContainer />
+        </div>
+    );
+}
+
+export default Login;
